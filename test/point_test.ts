0772a2d0995ffaaeedc6b3c8ed9e436328050f92
CrazyMhd <mahdi@teleportdao.xyz>
2023-04-18 13:49:13 +0300

    Adding the test with an unstandard way

--- test/point_test.ts
+++ test/point_test.ts
@@ -1,4 +1,4 @@
-import { BN } from 'bn.js'; // or whichever library you are using for big integers
+// import { BN } from 'bn.js'; // or whichever library you are using for big integers
 import { expect } from "chai";
 import { point, pointAdd } from '../src/points';
 import { Fp, Fp1 } from '../src/fields';
@@ -6,18 +6,15 @@ import { BigNumber } from '@ethersproject/bignumber';
 
 const g1TestVector = require("./fixtures/g1_add.json")
 
-describe('pointAdd', () => {
+function createG1Point(xStr: string, yStr: string): point {
+    return new point(
+        new Fp1(BigNumber.from(xStr)),
+        new Fp1(BigNumber.from(yStr)),
+        false
+    )
+}
 
-    function createG1Point(xStr: string, yStr: string): point {
-        return new point(
-            new Fp1(BigNumber.from(xStr)),
-            new Fp1(BigNumber.from(yStr)),
-            false
-        )
-    }
-      
-
-  it('g1 add', async function () {
+function g1AddTest() {
     for (let i = 0; i < g1TestVector.g1_add.length; i++) {
         let p1 = createG1Point(
             g1TestVector.g1_add[i].p1X,
@@ -40,6 +37,6 @@ describe('pointAdd', () => {
             res.eq(p1PlusP2)
         ).to.equal(true)
     }
-  });
+}
 
-});
+g1AddTest()
